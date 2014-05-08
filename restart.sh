e5c8b40c7f51ac1b5190e10de18bf01172b99a30
Ivan Neto <ivan.cr.neto@gmail.com>
2014-05-08 01:55:14 -0300

    changed port for killing process when restarting

--- restart.sh
+++ restart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-pid=`ps aux | grep fcgi | grep port=80888`;
+pid=`ps aux | grep fcgi | grep port=8099`;
 arr=(`echo $pid | cut -d " "  --output-delimiter=" " -f 1-`);
 echo "pid: ${arr[1]}";
 kill -9 ${arr[1]};
